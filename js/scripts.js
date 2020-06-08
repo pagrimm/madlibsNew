044e11254d4aca4151cd853745928b4f08ed9500
Thomas Glenn <thomasgrantglenn@mac.com>
2020-06-08 13:14:47 -0600

    add loop and array to .js.
    co-authored-by: Peter Grimm <pagrimm@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,37 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+    event.preventDefault();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    // const person1Input = $("input#person1").val();
+    // const person2Input = $("input#person2").val();
+    // const animalInput= $("input#animal").val();
+    // const exclamationInput = $("input#exclamation").val();
+    // const verbInput = $("input#verb").val();
+    // const nounInput = $("input#noun").val();
 
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
+
+    let inputs = [];
+    $("input").each(function(){
+      inputs.push($(this).val());
+    })
+    let position = 1;
+    inputs.forEach(function(element) {
+      $("span.input" + position).text(element);
+      position += 1
+    });
+    
     $("#story").show();
 
-    event.preventDefault();
+    }
+
   });
 });
+
+
+
